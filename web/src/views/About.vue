924fa6956f23166061d727a1e75db1a2eb1d6480
Yuan <396009575@qq.com>
2021-08-06 15:16:47 +0800

    1

---
+++ web/src/views/About.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="about">
+		<h1>{{obj.item.arr}}</h1>
+		<button @click="addArr">添加数组</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				obj: {
+					item:{
+						arr: [1,2,3]
+					}
+				}
+			}
+		},
+		methods: {
+			addArr() {
+				// console.log(this.obj.item.arr)
+				this.obj.item.arr.push(this.obj.item.arr[this.obj.item.arr.length-1]+1)
+				// this.obj.item.arr.push(arr[arr.length-1]+1)
+			}
+		},
+
+		watch: {
+			
+			obj:{
+				deep:true,
+				handler(newValue, oldValue){
+					console.log(newValue, oldValue)
+				}
+			}
+		}
+	}
+</script>
